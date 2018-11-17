1c3f6c300ee84a9be69e73d23bd83941296dcd02
Nic Bonetto <nick.bonetto@gmail.com>
2018-11-17 10:11:04 -0800

    Initial commit
    Signed-off-by: Nic Bonetto <nick.bonetto@gmail.com>

---
+++ source/server/index.js
@@ -0,0 +1,19 @@
+if (!require('dotenv').load()) {
+  console.error(`No .env file found. Make sure you have a .env file in the root directory`);
+  process.exit(1);
+}
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const helmet = require('helmet');
+const path = require('path');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet);
+
+const listener = app.listen(process.env.PORT, () => {
+  console.log(`Server is running on http://${listener.address().address}:${listener.address().port}`);
+});
