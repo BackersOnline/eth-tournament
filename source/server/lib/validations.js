8c8967da6541fd4754628bec034109acd5200a6a
Nic Bonetto <nick.bonetto@gmail.com>
2018-11-18 08:24:34 -0800

    server/login: Added login feature.
    Signed-off-by: Nic Bonetto <nick.bonetto@gmail.com>

--- source/server/lib/validations.js
+++ source/server/lib/validations.js
@@ -3,7 +3,11 @@ const registerSchema = {
     in: ['body'],
     isString: true,
     optional: false,
-    errorMessage: 'username is required and must be a string'
+    errorMessage: 'username is required and must be a string',
+    isLength: {
+      options: { min: 4, max: 20 },
+      errorMessage: 'username must be between 4 and 20 characters'
+    }
   },
   email: {
     in: ['body'],
@@ -15,22 +19,56 @@ const registerSchema = {
     in: ['body'],
     isString: true,
     optional: false,
-    errorMessage: 'password is required'
+    errorMessage: 'password is required',
+    isLength: {
+      options: { min: 8, max: 70 },
+      errorMessage: 'password must be between 8 and 70 characters'
+    }
   },
   first_name: {
     in: ['body'],
     isString: true,
     optional: false,
-    errorMessage: 'first_name is required'
+    errorMessage: 'first_name is required',
+    isLength: {
+      options: { min: 2, max: 30 },
+      errorMessage: 'first_name must be between 2 and 30 characters'
+    }
   },
   last_name: {
     in: ['body'],
     isString: true,
     optional: false,
-    errorMessage: 'last_name is required'
+    errorMessage: 'last_name is required',
+    isLength: {
+      options: { min: 2, max: 30 },
+      errorMessage: 'last_name must be between 2 and 30 characters'
+    }
+  }
+};
+
+const loginSchema = {
+  password: {
+    in: ['body'],
+    isString: true,
+    optional: false,
+    errorMessage: 'password is required'
+  },
+  email: {
+    in: ['body'],
+    isEmail: true,
+    optional: true,
+    errorMessage: 'invalid email'
+  },
+  username: {
+    in: ['body'],
+    isString: true,
+    optional: true,
+    errorMessage: 'username must be a string'
   }
 };
 
 module.exports = {
-  registerSchema
+  registerSchema,
+  loginSchema
 };
--- source/server/routes/auth.js
+++ source/server/routes/auth.js
@@ -1,9 +1,11 @@
 const registerController = require('../controllers/auth/register');
+const loginController = require('../controllers/auth/login');
 const { checkSchema } = require('express-validator/check');
-const { registerSchema } = require('../lib/validations');
+const { registerSchema, loginSchema } = require('../lib/validations');
 
 const authRoutes = (app) => {
   app.post('/auth/signup', checkSchema(registerSchema), registerController);
+  app.post('/auth/login', checkSchema(loginSchema), loginController);
 };
 
 module.exports = authRoutes;
